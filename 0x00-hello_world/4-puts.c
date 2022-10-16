c8a2b794b312f7601c3e5233d236e15b3084e7aa
kitingu <benlegendj@gmail.com>
2022-10-16 11:00:27 +0300

    intro to C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints string
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+/*
+ *Return: 0 after printing the function
+ */
+return (0);
+}
