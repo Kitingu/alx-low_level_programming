df57a47035f8afcfa3091fa28d7d47faba788c45
kitingu <benlegendj@gmail.com>
2022-10-18 19:15:15 +0300

    intro to nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints alphabet in lowercase
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints alphabet in lowercase
+ * Return: Always 0.
+ */
+int _islower(int c)
+
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
