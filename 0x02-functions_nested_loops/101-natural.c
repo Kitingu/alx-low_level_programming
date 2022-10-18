325cc6616bdb057607e9d7d30529a27a4efd7f93
kitingu <benlegendj@gmail.com>
2022-10-18 21:46:27 +0300

    fibonacci sequence

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  main entry
+ * Description: prints _putchar, followed by a new line
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char s[9] = "_putchar";
+
+int i = 0;
+
+	while (i < 8)
+{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
